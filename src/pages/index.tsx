94d1d960dc579fffd426ff3e68651303fec4cd5c
diegoogprado <diegoogprado@hotmail.com>
2024-08-14 15:28:52 -0300

    Aplicação finalizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import Header from '@/components/Header'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import { Button, Container } from 'reactstrap'
 
 const Home: NextPage = () => {
   return (
@@ -10,14 +13,22 @@ const Home: NextPage = () => {
         <link rel="icon" href='/favicon.ico' />
       </Head>
 
-      <main>
-        <h1>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
+      <Header />
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid perferendis cum odio tenetur voluptatibus consectetur harum, asperiores eligendi deleniti quam nulla, hic exercitationem dolorum dolorem sapiente itaque possimus esse. Quo architecto quis quas? Quisquam voluptates in facere nihil dolorem nostrum?
-        </p>
+      <main >
+        <Container className="py-5 text-center">
+          <h1 className="mt-5 display-1">
+            O melhor jeito de comprar o que você ama
+          </h1>
+          <p className="my-4">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae iusto voluptatem obcaecati omnis error architecto neque cum exercitationem fugiat. Vero illo autem eum nisi sapiente, odio optio accusamus cupiditate ad.
+          </p>
+          <Link href="/products">
+            <Button color="dark" className="px-4 pb-2">
+              Conheça nossos produtos!
+            </Button>
+          </Link>
+        </Container>
       </main>
     </>
   )
